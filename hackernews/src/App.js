5b61cec310e72b5a9ae85d15886ee11f5b053262
Salman Maarouf <salman.k.maarouf@gmail.com>
2025-07-05 14:17:25 +1000

    up and till page 25

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// defining the list
+const list = [
+  {
+    title: 'React',
+    url:'//x.com/jordwalke?lang=en',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://x.com/acdlite',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1,
+  },
+];
+
+
+
+class App extends Component {
+  render() {
+    const helloWorld = 'Welcome to the Road to learn React';
+    return (
+      <div className="App">
+
+       {list.map(item => 
+          <div key = {item.objectID}>
+            <span>
+              <a href={item.url}> {item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
